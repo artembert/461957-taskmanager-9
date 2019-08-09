56a8895ab7ce52460a08582c5c92b5645e5d6c5f
Artem Beresnev <aberesnev@intermedia.net>
2019-08-09 20:22:15 +0300

    add "webpack-dev-server"

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require(`path`);
 
 module.exports = {
-  mode: `development`,
+  mode: 'development',
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: 'http://localhost:8080/',
+    watchContentBase: true
+  }
 };
