a7fca1e0e2085401c956f156050a43f131dd944f
Artem Beresnev <aberesnev@intermedia.net>
2019-08-30 18:55:01 +0300

    add function createElement()

--- src/util/dom.js
+++ src/util/dom.js
@@ -2,7 +2,13 @@ import {Position} from "../models/position";
 
 export function render(markup, container, place = Position.BEFOREEND) {
   container.insertAdjacentHTML(place, markup);
-};
+}
+
+export function createElement(template) {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+}
 
 export function unrender(element) {
   if (element) {
