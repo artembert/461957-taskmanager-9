8d5caca237fea83849686c4bbb705361458157ea
Artem Beresnev <aberesnev@intermedia.net>
2019-08-30 19:59:40 +0300

    TaskEdit: generate markup for tags

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -143,50 +143,12 @@ export default class TaskEdit extends Task {
 
           <div class="card__hashtag">
             <div class="card__hashtag-list">
-                        <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <p class="card__hashtag-name">
-                            #repeat
-                          </p>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>
-
-              <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <p class="card__hashtag-name">
-                            #cinema
-                          </p>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>
-
-              <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <p class="card__hashtag-name">
-                            #entertaiment
-                          </p>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>
+              ${Array.from(this._tags).map((tag) => `<span class="card__hashtag-inner">
+                <span class="card__hashtag-inner">
+                  <input type="hidden" name="hashtag" value="${tag}" class="card__hashtag-hidden-input"/>
+                  <p class="card__hashtag-name">#${tag}</p>
+                  <button type="button" class="card__hashtag-delete">delete</button>
+                </span>`.trim()).join(``)}
             </div>
 
             <label>
