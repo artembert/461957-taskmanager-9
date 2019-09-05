9efe448c52f64ff9d4be6c5b4787344b3e693fbb
Artem Beresnev <artembert@gmail.com>
2019-09-05 22:14:43 +0300

    Homework have done in previous PRs

--- src/components/days-edit.js
+++ src/components/days-edit.js
@@ -9,8 +9,7 @@ export default class DaysEdit extends BaseComponent {
   getTemplate() {
     return `${this._repeatingDays.map(([day, isRepeating]) => `<input
       class="visually-hidden card__repeat-day-input" type="checkbox" name="repeat"
-      id="repeat-${day}-4" value="${day}"
-      ${isRepeating ? `checked` : ``}/>
+      id="repeat-${day}-4" value="${day}" ${isRepeating ? `checked` : ``}/>
       <label class="card__repeat-day" for="repeat-${day}-4">${day}</label>`).join(``)}`;
   }
 }
